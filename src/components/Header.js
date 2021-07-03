5b1f172731a96909dca4fd5aa85306f5893f5805
Sadaf <sadaf_cu@hotmail.com>
2021-07-03 11:55:21 +0500

    add header and balance components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <h1>
+            React Expense Tracker by Sadaf Siddiqui
+        </h1>
+    )
+}
+
+export default Header;
